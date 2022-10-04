855f7563a6343f2efbfe8b1456e6a7bac3066fe8
konrad <konrad@scandit.com>
2022-10-04 09:23:07 +0200

    Added styles

---
+++ app/theme/styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    label: {
+      color: 'white',
+      margin: 20,
+      marginLeft: 0,
+    },
+    button: {
+      marginTop: 40,
+      color: 'white',
+      height: 40,
+      backgroundColor: '#ec5990',
+      borderRadius: 4,
+    },
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      padding: 8,
+      backgroundColor: '#0e101c',
+    },
+    input: {
+      backgroundColor: 'white',
+      height: 40,
+      padding: 10,
+      borderRadius: 4,
+    },
+  });
